17ce9d49d022080272915ce726f8e2be32764310
Corey Bruyere <coreybruyere@ymail.com>
2020-07-07 13:22:25 -0700

    Make posts plural

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,8 +16,8 @@ module.exports = {
     {
       resolve: 'gatsby-source-filesystem',
       options: {
-        name: 'content',
-        path: `${__dirname}/src/content`
+        name: 'posts',
+        path: `${__dirname}/src/content/posts`
       }
     },
     {
