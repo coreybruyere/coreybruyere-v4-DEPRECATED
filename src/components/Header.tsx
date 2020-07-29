3e0258263986e3294439600f1cded2659cedf804
Corey Bruyere <coreybruyere@ymail.com>
2020-07-29 09:20:58 -0700

    Move header away from old theme spec

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,59 +1,47 @@
 import * as React from 'react'
 /** @jsx jsx */
-import { jsx } from 'theme-ui'
-import styled from '@emotion/styled'
-import { transparentize } from 'polished'
+import { jsx, Box, Flex } from 'theme-ui'
 import { Link } from 'gatsby'
 
-import { heights, dimensions, colors } from '../styles/variables'
 import Container from './Container'
-
-const StyledHeader = styled.header`
-  height: ${heights.header}px;
-  padding: 0 ${dimensions.containerPadding}rem;
-  background-color: ${colors.brand};
-  color: ${transparentize(0.5, colors.white)};
-`
-
-const HeaderInner = styled(Container)`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  height: 100%;
-`
-
-const HomepageLink = styled(Link)`
-  color: ${colors.white};
-  font-size: 1.5rem;
-  font-weight: 600;
-
-  &:hover,
-  &:focus {
-    text-decoration: none;
-  }
-`
 
 interface HeaderProps {
   title: string
 }
 
 const Header: React.FC<HeaderProps> = ({ title }) => (
-  <StyledHeader>
-    <div
+  <Box px={4} sx={{ backgroundColor: 'muted' }}>
+    <Box
+      p={4}
       sx={{
-        // this uses the value from `theme.space[4]`
-        padding: 4,
         // these use values from `theme.colors`
         color: 'background',
         backgroundColor: 'primary'
       }}
     >
       hi
-    </div>
-    <HeaderInner>
-      <HomepageLink to="/">{title}</HomepageLink>
-    </HeaderInner>
-  </StyledHeader>
+    </Box>
+    <Flex
+      sx={{
+        flexDirection: 'row',
+        alignItems: 'center',
+        height: '100%'
+      }}
+    >
+      <Link
+        activeClassName="active"
+        sx={{
+          color: 'inherit',
+          '&.active': {
+            color: 'primary'
+          }
+        }}
+        to="/"
+      >
+        {title}
+      </Link>
+    </Flex>
+  </Box>
 )
 
 export default Header
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,15 +1,12 @@
 export default {
-  colors: {
-    text: '#333',
-    background: '#fff',
-    primary: 'red',
-    secondary: '#ff6347'
-  },
+  breakpoints: ['40em', '52em', '64em'],
+  space: [0, 4, 8, 16, 24, 32, 64, 128, 256, 512],
   fonts: {
-    body: 'system-ui, sans-serif',
-    heading: 'system-ui, sans-serif',
+    body: 'system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", sans-serif',
+    heading: 'inherit',
     monospace: 'Menlo, monospace'
   },
+  fontSizes: [12, 14, 16, 20, 24, 32, 48, 64, 72],
   fontWeights: {
     body: 400,
     heading: 700,
@@ -19,5 +16,73 @@ export default {
     body: 1.5,
     heading: 1.125
   },
-  fontSizes: [12, 14, 16, 20, 24, 32, 48, 64, 72]
+  colors: {
+    text: '#000',
+    background: '#fff',
+    primary: '#07c',
+    secondary: '#30c',
+    muted: '#f6f6f6'
+  },
+  text: {
+    heading: {
+      fontFamily: 'heading',
+      lineHeight: 'heading',
+      fontWeight: 'heading'
+    }
+  },
+  styles: {
+    root: {
+      fontFamily: 'body',
+      lineHeight: 'body',
+      fontWeight: 'body'
+    },
+    h1: {
+      variant: 'text.heading',
+      fontSize: 5
+    },
+    h2: {
+      variant: 'text.heading',
+      fontSize: 4
+    },
+    h3: {
+      variant: 'text.heading',
+      fontSize: 3
+    },
+    h4: {
+      variant: 'text.heading',
+      fontSize: 2
+    },
+    h5: {
+      variant: 'text.heading',
+      fontSize: 1
+    },
+    h6: {
+      variant: 'text.heading',
+      fontSize: 0
+    },
+    pre: {
+      fontFamily: 'monospace',
+      overflowX: 'auto',
+      code: {
+        color: 'inherit'
+      }
+    },
+    code: {
+      fontFamily: 'monospace',
+      fontSize: 'inherit'
+    },
+    table: {
+      width: '100%',
+      borderCollapse: 'separate',
+      borderSpacing: 0
+    },
+    th: {
+      textAlign: 'left',
+      borderBottomStyle: 'solid'
+    },
+    td: {
+      textAlign: 'left',
+      borderBottomStyle: 'solid'
+    }
+  }
 }
